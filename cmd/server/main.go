86c95a8d42277f08a260855289a5d5991da3dbd9
Zev Solomon <zev2888@gmail.com>
2024-06-05 15:58:13 +0900

    Chapter 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+    connStr := "amqp://guest:guest@localhost:5672"
+    conn, err := amqp.Dial(connStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+    fmt.Printf("Connection opened: %v", connStr)
 	fmt.Println("Starting Peril server...")
+
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, os.Interrupt)
+    <-signalChan
+
+    fmt.Println("Shutting down")
+    conn.Close()
 }
